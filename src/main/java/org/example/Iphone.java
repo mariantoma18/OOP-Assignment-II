ff0a76854a5a8385d4df670b9021fc78af74f3bd
MARIANTOMA\Marian <marian.toma2818@gmail.com>
2024-12-16 19:36:39 +0200

    commit 1

---
+++ src/main/java/org/example/Iphone.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public abstract class Iphone extends Phone {
+
+  private String color;
+  private String material;
+
+  public Iphone(String material, String color) {
+    this.material = material;
+    this.color = color;
+  }
+
+  /*
+   *Method that make a phone call (Printing the phone number) and adding the phone number to an ArrayList of calls
+   */
+  @Override
+  public void call(String callPhoneNumber) {
+    System.out.println(
+        "You have called the phone number: " + callPhoneNumber + " from your Iphone.");
+    calls.add(callPhoneNumber);
+  }
+
+  /*
+   * Method that sends a message based on the phone number;
+   * The putIfAbsent method is used in case that the phone number does not exist in the phoneMessages HashMap;
+   * That creates a new ArrayList (to add messages) for the provided phone number.
+   * If the phone number already exist in the Map, the method is ignored.
+   */
+  @Override
+  public void sendMessage(String phoneNumber, String messageContent) {
+    if (messageContent.length() > 100) {
+      System.out.println("Message too long\nMaximum 100 chars allowed!");
+      return;
+    }
+
+    phoneMessages.putIfAbsent(phoneNumber, new ArrayList<>());
+
+    phoneMessages.get(phoneNumber).add(messageContent);
+  }
+
+  /*
+   *Method used to get the first message from a certain phone number
+   */
+  public void getFirstMessage(String phoneNumber) {
+    ArrayList<String> messages = phoneMessages.get(phoneNumber);
+    System.out.println(messages.get(0));
+  }
+
+  /*
+   *Method used to get the second message from a certain phone number
+   */
+  public void getSecondMessage(String phoneNumber) {
+    ArrayList<String> messages = phoneMessages.get(phoneNumber);
+    System.out.println(messages.get(1));
+  }
+
+  /*
+   *Method used to get a certain message from a certain phone number
+   */
+  public void getCertainMessage(String phoneNumber, int index) {
+    ArrayList<String> messages = phoneMessages.get(phoneNumber);
+    System.out.println(messages.get(index - 1));
+  }
+
+  /*
+   * Method used to print the history of calls
+   */
+  public void viewHistory() {
+    int index = 1;
+    System.out.println("---Call History---\n");
+    for (String call : calls) {
+      System.out.println(index + ": " + call);
+      index++;
+    }
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public String getMaterial() {
+    return material;
+  }
+}
---
+++ src/main/java/org/example/TextAndCallFeatures.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface TextAndCallFeatures {
+
+  void call(String phoneNumber);
+  void sendMessage(String phoneNumber, String messageContent);
+}
